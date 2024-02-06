ad8fbeaca321429744ac16a19e8ed02ad7d5e7d2
R4D3V <raymonjohns@gmail.com>
2024-02-06 15:22:14 +0300

    main

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,17 +6,17 @@ function About() {
     <div className="p-4 font-mono">
         <div className="pb-4 font-bold">
             <h1 className="text-3xl text-center ">
-            Welcome to [Agency Name] – Crafting Digital Excellence
+            Welcome to REED-MEDIA – Crafting Digital Excellence
             </h1>
             <p className=" py-3 px-4">
-            At [Agency Name], we don't just build websites; we create digital experiences that leave a lasting impression. As a leading web development agency, we specialize in transforming your ideas into stunning, functional, and user-friendly online solutions.
+            At REED-MEDIA, we don't just build websites; we create digital experiences that leave a lasting impression. As a leading web development agency, we specialize in transforming your ideas into stunning, functional, and user-friendly online solutions.
             </p>
             <p className=" py-3 px-4">
             Our team of skilled developers, designers, and strategists collaborate seamlessly to bring your vision to life. From sleek corporate websites to robust e-commerce platforms, we tailor our services to meet your unique goals. Embrace innovation with cutting-edge technologies, responsive design, and scalable solutions that stand the test of time.   
             </p>
         </div>
         <h1 className="py-6 text-4xl text-center">
-        Why choose [Agency Name]?
+        Why choose REED-MEDIA?
         </h1>
          <div className="p-4">
          <h1 className="text-8xl absolute dark:text-gray-700 text-violet-400 font-bold z-1" >
@@ -75,7 +75,7 @@ function About() {
          </div>
          
         <h2>
-        Explore our portfolio and see the transformative impact we've had on businesses like yours. Ready to elevate your online presence? Contact [Agency Name] today and let's embark on a journey to digital excellence together.
+        Explore our portfolio and see the transformative impact we've had on businesses like yours. Ready to elevate your online presence? Contact REED-MEDIA today and let's embark on a journey to digital excellence together.
         </h2>
     </div>
   )
